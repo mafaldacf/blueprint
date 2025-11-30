e52c80007a997bd885c067de6181e595fd5c5d94
Mafalda Ferreira <mafaldasofiacf@gmail.com>
2025-11-30 16:41:59 +0000

    update postnotification

--- examples/postnotification_simple/workflow/postnotification_simple/NotifyService.go
+++ examples/postnotification_simple/workflow/postnotification_simple/NotifyService.go
@@ -6,8 +6,6 @@ import (
 	"github.com/blueprint-uservices/blueprint/runtime/core/backend"
 )
 
-// does not expose any methods to other services
-// it defines Run that runs workers that pull messages from the notificationsQueue
 type NotifyService interface {
 	Run(ctx context.Context) error
 }
@@ -15,45 +13,30 @@ type NotifyService interface {
 type NotifyServiceImpl struct {
 	storageService     StorageService
 	notificationsQueue backend.Queue
-	num_workers        int
+	exit_on_error      bool
 }
 
 func NewNotifyServiceImpl(ctx context.Context, storageService StorageService, notificationsQueue backend.Queue) (NotifyService, error) {
-	n := &NotifyServiceImpl{storageService: storageService, notificationsQueue: notificationsQueue, num_workers: 4}
+	n := &NotifyServiceImpl{storageService: storageService, notificationsQueue: notificationsQueue, exit_on_error: false}
 	return n, nil
 }
 
 func (n *NotifyServiceImpl) Run(ctx context.Context) error {
-	backend.GetLogger().Info(ctx, "initializing %d workers", n.num_workers)
-
-	/* var wg sync.WaitGroup
-	wg.Add(n.num_workers) */
-
 	for {
-		var workerMessage Message
-		ok, err := n.notificationsQueue.Pop(ctx, &workerMessage)
-		if err != nil {
-			return err
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			var workerMessage Message
+			ok, err := n.notificationsQueue.Pop(ctx, &workerMessage)
+			if err != nil && n.exit_on_error {
+				return err
+			}
+			if !ok {
+				continue
+			}
+			n.storageService.ReadPost(ctx, workerMessage.ReqID, workerMessage.PostID)
 		}
-		if !ok {
-			continue
-		}
-		n.storageService.ReadPost(ctx, workerMessage.ReqID, workerMessage.PostID)
 	}
-
-	/* fn := func(i int) {
-		defer wg.Done()
-
-		var workerMessage Message
-		n.notificationsQueue.Pop(ctx, &workerMessage)
-		n.storageService.ReadPost(ctx, workerMessage.ReqID, workerMessage.PostID)
-	}
-
-	for i := 1; i <= n.num_workers; i++ { // for
-		go fn(i)
-	}
-	wg.Wait() */
-
-	backend.GetLogger().Info(ctx, "joining %d workers", n.num_workers)
 	return nil
 }
